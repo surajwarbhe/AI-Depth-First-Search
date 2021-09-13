9c95c74b3381cda547d43777eb512c5c201bc1e8
Suraj Warbhe <67333590+surajwarbhe@users.noreply.github.com>
2021-09-13 23:13:55 +0530

    Create DFS.py

---
+++ DFS.py
@@ -0,0 +1,27 @@
+# DSF ASSIGNMENT
+# Create a graph
+Graph = {
+    'A': ['B', 'D'],
+    'B': ['C', 'F'],
+    'C': ['E', 'G', 'H'],
+    'G': ['E', 'H'],
+    'E': ['B', 'F'],
+    'F': ['A'],
+    'D': ['F'],
+    'H': ['A'],
+}
+
+# function of DFS
+def DFS(Graph, node, visited):
+    if node not in visited:
+        visited.append(node)
+        for n in Graph[node]:
+            DFS(Graph, n, visited)
+    return visited
+
+
+start = input("Enter starting vertex : ")
+print("The Depth-First Search of the following Graph: ") visited = DFS(Graph, start, [])
+
+for i in visited:
+    print(i, end=' ')
